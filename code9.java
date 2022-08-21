d14999cf13e54e40d59e100b5cf320ce19695c88
Aritra203 <aritra.chakraborty203@gmail.com>
2022-08-22 00:25:30 +0530

    Modified Code

--- code9.java
+++ code9.java
@@ -1,7 +1,16 @@
+
+import java.util.Scanner;
 public class code9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am coding very well");
+			Scanner sc=new Scanner(System.in);
+ 			System.out.print("Enter your age  :-  ");
+			int age=sc.nextInt();
+			System.out.println();
+			if(age>=18)
+				System.out.println("You can Vote");
+			else
+				System.out.println("You can't Vote");		
 		}
 	}
